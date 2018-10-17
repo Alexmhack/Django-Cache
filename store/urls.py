fc5bc3759d8ce6cad7d8957907f98638cf5456a1
alexHooper <alexmohack@gmail.com>
2018-10-17 10:43:03 +0530

    add view for cached products

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import view_products
+from .views import view_products, view_cached_products
 
 app_name = 'store'
 
 urlpatterns = [
 	path('', view_products, name='products'),
+	path('cache/', view_cached_products, name='cached-products'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.core.cache import cache
+from django.conf import settings
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -6,8 +9,27 @@ from rest_framework import status
 
 from .models import Product
 
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
+
 @api_view(['GET'])
 def view_products(request):
 	products = Product.objects.all()
 	results = [product.to_json() for product in products]
 	return Response(data=results, status=status.HTTP_201_CREATED)
+
+
+# view for cached products
+@api_view(['GET'])
+def view_cached_products(request):
+	if 'product' in cache:
+		print("True")
+		products = cache.get('product')
+		return Response(data=products, status=status.HTTP_201_CREATED)
+	else:
+		print("False")
+		products = Product.objects.all()
+		results = [product.to_json() for product in products]
+
+		# store products in cache
+		cache.set(products, results, timeout=CACHE_TTL)
+		return Response(data=results, status=status.HTTP_201_CREATED)
