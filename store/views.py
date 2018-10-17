731ebe97d6e56e88f12e1ffc37d4569fac01f930
alexHooper <alexmohack@gmail.com>
2018-10-17 10:12:58 +0530

    correct the spell error

--- store/views.py
+++ store/views.py
@@ -8,6 +8,6 @@ from .models import Product
 
 @api_view(['GET'])
 def view_products(request):
-	products = Products.objects.all()
+	products = Product.objects.all()
 	results = [product.to_json() for product in products]
 	return Response(data=results, status=status.HTTP_201_CREATED)
